f909c84049e2f37ee08cf83b9d8e5541dd22e812
zaharmihed <zahar.mihed@gmail.com>
2026-05-06 19:59:47 +0300

    практика 1

---
+++ src/by/class1/Lesson1.java
@@ -0,0 +1,48 @@
+package by.class1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    public static void main(String[] args){
+        int x;
+        x = 88/(10+12);
+        System.out.println(x);
+        System.out.println("**************");
+        /*
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите a");
+        int a = sc.nextInt();
+        System.out.println("Введите y");
+        int y = sc.nextInt();
+        System.out.println("Введите z");
+        int z = sc.nextInt();
+
+        int resulr = a*(y*z);
+        System.out.println("Результат: " + resulr);
+
+
+         */
+        /*
+        int b;
+        b = (213+258)/(217-60);
+        System.out.println("Результат: " + b);
+
+         */
+
+        int i = 5;
+        int j = 2;
+        int c;
+        c = i++ * j--;
+        System.out.println("Результат: " + c);
+        System.out.println(i);
+        System.out.println(j);
+
+
+
+
+
+    }
+
+
+}
---
+++ src/by/class1/Lesson1_2.java
@@ -0,0 +1,46 @@
+package by.class1;
+
+import java.util.Scanner;
+
+public class Lesson1_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите x");
+        int x = sc.nextInt();
+        System.out.println("Введите y");
+        int y = sc.nextInt();
+        System.out.println("Введите z");
+        int z = sc.nextInt();
+
+
+        int a = 3;
+        int b = 10;
+        int c;
+        /*
+        if ( a > b ) {
+            c = a - b;
+            System.out.println(c);
+        } else if (a == b) {
+            c = a + b;
+            System.out.println(c);
+        } else {
+            c = b - a;
+            System.out.println(c);
+
+        }
+
+    }
+
+ */
+        /*if (a%3==0) {
+            System.out.println("Число а делится на 3");
+        } else System.out.println("Число а не делится на 3");*/
+
+        if (a % 3 == 0 && a % 7 == 0) {
+
+        }
+
+
+        }
+
+    }
